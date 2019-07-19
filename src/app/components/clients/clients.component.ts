fde332c47f2c02d303e1f5bd634027ea3c72bfdf
Annela <Linga.X.Annela@questdiagnostics.com>
2019-07-19 15:51:26 -0400

    Added Modal tamplate

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -11,6 +11,8 @@ import { ManageclientDialogComponent } from './manageclient-dialog/manageclient-
 
 export class ClientsComponent implements OnInit {
   public clients : Client[];
+  public height: string;
+  public width: string;
 
   constructor(public dialog: MatDialog) { }
 
@@ -26,17 +28,41 @@ export class ClientsComponent implements OnInit {
   ];
   }
 
-  openDialog(): void {
+  openDialog(data, height, width): void {
     const dialogRef = this.dialog.open(ManageclientDialogComponent, {
-      height: '400px',
-      width: '600px',
-      data: {id: "12345", firstName: "Linga", lastName: "Annela", email : "alreddy@ashta.com", phoneNo: "987-987-1230" }
+      height: (height) ? height : '400px',
+      width: (width) ? width : '600px',
+      data: (data) ? data : 'viewClicked'
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
       
     });
+  }
+
+  onNewClientAdd(){
+    this.height = '400px';
+    this.width = '600px';
+    // Need to send the information as well.
+    let data = 'newClicked';
+    this.openDialog(data, this.height, this.width);
+  }
+
+  onEditClient() {
+    this.height = '400px';
+    this.width = '600px';
+    // Need to send the information as well.
+    let data = 'editClicked';
+    this.openDialog(data, this.height, this.width);
+  }
+
+  onDeleteClient() {
+    this.height = '400px';
+    this.width = '400px';
+    // Need to send the information as well.
+    let data = 'deleteClicked';
+    this.openDialog(data, this.height, this.width);
   }
 
 }
