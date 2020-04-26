f084971b91904ada3bba547ea2820a4089f9385a
MacBook <lingareddy.al@gmail.com>
2020-04-26 18:43:15 -0400

    CSS Grid Chitfund

--- src/app/chitfund/chitfund.module.ts
+++ src/app/chitfund/chitfund.module.ts
@@ -4,11 +4,13 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { ChitfundsComponent } from './components/chitfunds.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialModule } from '../shared/material.module';
 
 const ROUTES: Routes = [{ path: '', component: ChitfundsComponent }];
 @NgModule({
     imports: [
       CommonModule,
+      MaterialModule,
       StoreModule.forFeature('chitfund', {}),
       EffectsModule.forFeature([]),
       RouterModule.forChild(ROUTES)
